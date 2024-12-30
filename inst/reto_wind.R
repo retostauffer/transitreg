f296a0fea3487740c4d3f2376aa16ef366874bcb
Reto Stauffer <reto.stauffer@uibk.ac.at>
2024-12-30 19:56:35 +0100

    test: Spatial Wmax

--- inst/reto_wind.R
+++ inst/reto_wind.R
@@ -12,8 +12,11 @@ library("rnaturalearth")
 data("WeatherGermany", package = "WeatherGermany")
 data <- subset(WeatherGermany, !is.na(Wmax))
 head(data)
+range(data$date)
 
-PERIOD <- list(start = as.Date("2015-01-01"), end = as.Date("2022-12-01"))
+##PERIOD <- list(start = as.Date("2015-01-01"), end = as.Date("2022-12-01"))
+PERIOD <- list(start = as.Date("2000-01-01"), end = as.Date("2022-12-01"))
+# --- period 2000-01-01 to 2022-12-01 with 100 breaks took 1484 seconds (42 minutes)
 x <- subset(data, date >= PERIOD$start & date <= PERIOD$end)
 x$year <- as.integer(format(x$date, "%Y"))
 counts <- x %>% group_by(id) %>% summarize(count = length(Wmax))
@@ -55,6 +58,10 @@ ggplot() +
 # Estimating the model
 f <- Wmax ~ ti(alt) + ti(lon, lat) + ti(year) + ti(alt, year)
 system.time(b <- tm(f, data = x, breaks = 100, useC = TRUE))
+saveRDS(b, "____b.rds")
+
+dim(b$model.frame)
+dim(model.frame(b$model))
 
 nd <- as.data.frame(dem)
 names(nd) <- c("lon", "lat", "alt")
@@ -63,7 +70,6 @@ nd$year <- 2022
 
 # ---------------------------------------------------------------
 
-devtools::load_all("../")
 p5  <- predict(b, newdata = nd, useC = TRUE, type = "pdf", y = 5)
 p12 <- predict(b, newdata = nd, useC = TRUE, type = "pdf", y = 12)
 
@@ -79,6 +85,7 @@ g12 <- ggplot() + geom_stars(data = dem12) +
     scale_fill_continuous_sequential("ag_sunset") +
     labs(title = "P(Wmax > 12)") + theme_minimal()
 g5 + g12
+ggsave(g5 + g12, filename = "__wind_p.png", width = 12, height = 6)
 
 # ---------------------------------------------------------------
 
@@ -87,6 +94,7 @@ names(demq50) <- "median"
 q50 <- predict(b, newdata = nd, type = "quantile", prob = 0.5, useC = TRUE)
 demq50[[1]] <- matrix(q50, ncol = ncol(demq50[[1]]))
 gq50 <- ggplot() + geom_stars(data = demq50) +
+           scale_fill_continuous_sequential("Hawaii") +
            labs(title = "Median") + theme_minimal()
 
 demq75 <- dem
@@ -94,9 +102,11 @@ names(demq75) <- "q75"
 q75 <- predict(b, newdata = nd, type = "quantile", prob = 0.9, useC = TRUE)
 demq75[[1]] <- matrix(q75, ncol = ncol(demq75[[1]]))
 gq75 <- ggplot() + geom_stars(data = demq75) +
+           scale_fill_continuous_sequential("Hawaii") +
            labs(title = "75er Percentile") + theme_minimal()
 
 gq50 + gq75
+ggsave(gq50 + gq75, filename = "__wind_q.png", width = 12, height = 6)
 
 
 
